9a5ef6f4ba0b0e355a3066d86a270adc63fa5779
Gregg <cgregg@belgacom.net>
2026-05-04 21:36:05 +0200

    feat(security): implement credential service for storing hashed member passwords

--- backend-nest/src/modules/security/services/credential.service.ts
+++ backend-nest/src/modules/security/services/credential.service.ts
@@ -1,8 +1,22 @@
-/*
-https://docs.nestjs.com/providers#services
-*/
-
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { Member } from "@member/entities";
+import { Credential } from "@security/entities"
+import { Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
 
 @Injectable()
-export class CredentialService { }
+export class CredentialService { 
+  constructor( @InjectRepository(Credential) private readonly credentialRepository: Repository<Credential>) {}
+
+  /**
+   * Creates a new credential for the given member.
+   */
+  async create(member: Member, hashedPassword: string): Promise<Credential> {
+    const credential = this.credentialRepository.create({
+      password: hashedPassword,
+      member,
+    });
+
+    return this.credentialRepository.save(credential);
+  }
+}
