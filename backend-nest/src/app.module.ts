14f879993a3bb82d45850eec9780698b2de115aa
Gregg <cgregg@belgacom.net>
2026-05-06 00:02:32 +0200

    chore(config): enable global ConfigModule in AppModule
    - Add ConfigModule.forRoot({ isGlobal: true })
    - Ensure environment variables are available across all modules
    - Fix missing JWT_SECRET during JwtModule initialization

--- backend-nest/src/app.module.ts
+++ backend-nest/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configManager } from '@core/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 import { MemberModule } from '@member/member.module';
 import { SecurityModule } from '@security/security.module';
@@ -12,6 +13,7 @@ import { AccountModule } from '@account/account.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(configManager.getTypeOrmConfig()),
+    ConfigModule.forRoot({ isGlobal: true }),
     MemberModule,
     AccountModule,
     SecurityModule
