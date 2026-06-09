4f3a11e6f442cc135aa30e3ac285a49de9ab8d07
Gregg <cgregg@belgacom.net>
2026-06-09 23:11:39 +0200

    feat(dashboard): update dashboard component (template and logic)

--- frontend/src/app/features/dashboard/dashboard.ts
+++ frontend/src/app/features/dashboard/dashboard.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
+import { AuthService } from '@core/auth/auth.service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.scss',
+  styleUrl: './dashboard.scss'
 })
-export class Dashboard {}
+export class Dashboard {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  
+  // Exemple avec Signaux pour la liste
+  recipes = signal([
+    { id: '1', title: 'Recette aux figues', description: 'Une merveille...' },
+    { id: '2', title: 'Toast au jambon', description: 'Le classique.' }
+  ]);
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
+  
+  addRecipe() { /* Logique d'ajout */ }
+  editRecipe(id: string) { /* Logique d'update */}
+  deleteRecipe(id: string) { /* Logique de suppression */ }
+}
