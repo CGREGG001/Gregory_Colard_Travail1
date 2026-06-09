7bbcfcc016d341dede7fb414f99e9e55cc561d00
Gregg <cgregg@belgacom.net>
2026-06-09 15:49:34 +0200

    refactor(config): update app configuration to enable zoneless change detection and HTTP client interceptors.
    - Added provideZonelessChangeDetection()
    - Added withFetch()

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,27 +1,19 @@
 import { 
   ApplicationConfig,
-  inject,
   provideAppInitializer,
+  provideZonelessChangeDetection,
   provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection
+  inject
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-
 import { AuthService } from '@core/auth/auth.service';
-import { of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { authInterceptor } from '@core/interceptors/auth.interceptor';
+import { catchError, of } from 'rxjs';
 
-/**
- * Factory function to refresh the token during app initialization.
- * This prevents losing the session on page refresh (F5).
- */
 function initializeApp() {
   const authService = inject(AuthService);
-
   return authService.refresh().pipe(
     catchError(() => of(null))
   );
@@ -31,8 +23,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideZonelessChangeDetection(), 
+    provideHttpClient(withInterceptors([authInterceptor]), withFetch()),
     provideAppInitializer(initializeApp),
   ]
 };
