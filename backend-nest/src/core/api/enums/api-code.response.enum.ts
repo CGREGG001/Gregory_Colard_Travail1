44239fa2ac15927176c6892ae1ef32b0f766176c
Gregg <cgregg@belgacom.net>
2026-06-18 11:14:30 +0200

    feat(api): add recipe error codes (not_found, forbidden_action)

--- backend-nest/src/core/api/enums/api-code.response.enum.ts
+++ backend-nest/src/core/api/enums/api-code.response.enum.ts
@@ -1,15 +1,15 @@
 export enum ApiCodeResponse {
-    // Succès
+    // Success
     COMMON_SUCCESS = 'api.success.common',
 
-    // Erreurs Communes
+    // Common errors
     PAYLOAD_IS_NOT_VALID = 'api.error.payload_is_not_valid',
 
-    // Erreurs d'Authentification (Security)
+    // Authentication errors (Security)
     USER_NOT_FOUND = 'api.error.user_not_found',
     USER_ALREADY_EXIST = 'api.error.user_already_exist',
     TOKEN_EXPIRED = 'api.error.token_expired',
-    NO_TOKEN_FOUNDED = 'api.error.no_token_founded',
+    NO_TOKEN_FOUND = 'api.error.no_token_found',
     SIGNUP_ERROR = 'api.error.signup_error',
     TOKEN_GEN_ERROR = 'api.error.token_gen_error',
     INVALID_CREDENTIALS = 'api.error.invalid_credentials',
@@ -17,5 +17,10 @@ export enum ApiCodeResponse {
     NICKNAME_ALREADY_EXISTS = 'api.error.nickname_already_exists',
     TOKEN_CREDENTIAL_MISSING = 'api.error.token_credential_missing',
 
+    // Member Errors
     MEMBER_NOT_FOUND = 'api.error.member_not_found',
+
+    // Recipe errors
+    RECIPE_NOT_FOUND = 'api.error.recipe_not_found',
+    RECIPE_FORBIDDEN_ACTION = 'api.error.recipe_forbidden_action'
 }
