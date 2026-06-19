d26a1d74edd5ab5054cba68e9536c89b51dc6fe9
Gregg <cgregg@belgacom.net>
2026-06-19 19:11:01 +0200

    feat(recipes): add create route for RecipeForm

--- frontend/src/app/features/recipe/recipe.routes.ts
+++ frontend/src/app/features/recipe/recipe.routes.ts
@@ -5,4 +5,8 @@ export const recipeRoutes: Routes = [
         path: '',
         loadComponent: () => import('./page/recipe-list/recipe-list').then(m => m.RecipeListComponent)
     },
+    {
+        path: 'create',
+        loadComponent: () => import('./page/recipe-form/recipe-form').then(m => m.RecipeForm)
+    },
 ];
