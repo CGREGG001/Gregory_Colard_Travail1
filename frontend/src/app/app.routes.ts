9e3b8c8eab3012f510db355a67ea8882603a6256
Gregg <cgregg@belgacom.net>
2026-06-20 02:38:19 +0200

    feat(routes): add members and member detail routes with adminGuard protection

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,19 @@
-import { Routes } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router, Routes } from '@angular/router';
+
+import { AuthService } from '@core/auth';
 import { authGuard, guestGuard } from '@core/guards/auth.guard';
+
+// TODO : refactoriser dans core/guards/admin.guard.ts
+const adminGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  
+  if (authService.currentUser()?.role === 'admin') {
+    return true;
+  }
+  return router.createUrlTree(['/dashboard/recipes']); // Redirige ailleurs si pas admin
+};
 
 export const routes: Routes = [
     {
@@ -34,6 +48,16 @@ export const routes: Routes = [
             {
                 path: 'profile',
                 loadComponent: () => import('@features/account/account').then(m => m.AccountComponent)
+            },
+            {
+                path: 'members',
+                canActivate: [adminGuard], // <-- Sécurisation Front-end !
+                loadComponent: () => import('@features/member/page/member-list/member-list').then(m => m.MemberList)
+            },
+            {
+                path: 'members/detail/:id',
+                canActivate: [adminGuard],
+                loadComponent: () => import('@features/member/page/member-detail/member-detail').then(m => m.MemberDetail)
             },
         ]
     },
